a9d1bd0b8bb5e336df41e77352a899cf0ab0c3c8
yohans <emailyyohans@gmail.com>
2025-06-19 19:40:26 +0300

    experment with fonts

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="text-h2 inconsolata-font">
+        Whereas disregard and contempt for human rights have resulted
+    </div>
+</template>
+
+<script setup></script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createVuetify } from 'vuetify'
+import * as components  from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+
+const vuetify = createVuetify({
+    components,
+    directives
+})
+
+createApp(App)
+    .use(vuetify)
+    .mount('#app')
