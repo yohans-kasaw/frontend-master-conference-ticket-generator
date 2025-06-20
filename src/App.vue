485d9414f3ccd14b6d7d0db929a46a069bc3995a
yohans <emailyyohans@gmail.com>
2025-06-20 18:56:09 +0300

    add first try background

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-h2 inconsolata-font">
+    <div class="d-flex justify-center align-center text-h4 fade-in" style="height: 100vh !important;">  
         Whereas disregard and contempt for human rights have resulted
     </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    server: {
+        allowedHosts: ['fifty-cougars-warn.loca.lt'],
+    },
 })
