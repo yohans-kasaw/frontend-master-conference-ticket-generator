1d65e3f37b9a6e7182e6711f3890ad3a01d830fd
yohans-kasaw <emailyohans@gmail.com>
2025-06-23 12:04:13 +0300

    added title and sub title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
-    <div class="d-flex justify-center align-center text-h4 fade-in" style="height: 100vh !important;">  
-        Whereas disregard and contempt for human rights have resulted
+    <div class="d-flex align-center flex-column ga-10">
+        <div class="pt-10">
+            <img :src="logUrl" />
+        </div>
+        <div
+            class="text-center font-weight-bold font-inconsolata"
+            style="font-size: 4rem; font-weight: 800"
+        >
+            Congrats, <span class="red-gradient-text">Jonatan Kirstof!</span>
+            <br />
+            Your ticket is ready
+        </div>
+        <div
+            class="text-center font-inconsolata"
+            style="font-size: 2rem; font-weight: 400"
+        >
+            We've emailed your ticket to
+            <br />
+            <span class="red-text">jonatan@email.com</span> and will send update
+            in
+            <br />
+            the run up to the event.
+        </div>
+        <div class="ticket-display">
+            <img :src="logUrl" />
+        </div>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import logUrl from './assets/svgs/logo-full.svg'
+</script>
 
 <style></style>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
     server: {
         allowedHosts: ['fifty-cougars-warn.loca.lt'],
     },
+    resolve: {
+        alias: {
+            '@src': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
 })
