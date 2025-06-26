88d6fa098c1cfd7343f91adc9150ad43221efce9
yohans-kasaw <emailyohans@gmail.com>
2025-06-26 14:26:07 +0300

    mobile: make tickat card usirg v-image, avoided fixed height and width

--- src/components/Ticket.vue
+++ src/components/Ticket.vue
@@ -1,46 +1,57 @@
 <template>
-    <div
-        class="ticket-background d-flex justify-space-between"
-        style="height: 280px; width: 600px"
-    >
-        <div class="pa-5 d-flex flex-column justify-space-between">
-            <div class="pt-2">
-                <v-img :src="log_full_url" :width="280" />
-                <div class="ml-16 font-mid" variant="text">
-                    {{ eventDate }} / {{ eventCity }}
+    <div>
+        <v-img :src="ticket_background_url">
+            <div
+                class="d-flex justify-space-between h-100"
+                style="gap: 80px;"
+            >
+                <div class="pa-6 d-flex flex-column justify-space-between">
+                    <div class="pt-2">
+                        <img :src="log_full_url" height="40"/>
+                        <div class="ml-16 font-mid" variant="text">
+                            {{ eventDate }} / {{ eventCity }}
+                        </div>
+                    </div>
+                    <div class="d-flex align-center ga-4">
+                        <v-avatar rounded="0" size="82">
+                            <v-img
+                                :src="userAvatarUrl"
+                                style="border-radius: 16px"
+                            />
+                        </v-avatar>
+                        <div>
+                            <div class="font-weight-medium font-big">
+                                {{ userName }}
+                            </div>
+                            <div class="d-flex ga-2">
+                                <img :src="icon_github" />
+                                <span class="font-weight-medium font-mid">
+                                    {{ userGithub }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class="d-flex justify-center align-center ml-4"
+                >
+                    <v-chip
+                        class="vertical-text text-h5"
+                        color="grey"
+                        variant="text"
+                    >
+                        {{ ticketNumber }}
+                    </v-chip>
                 </div>
             </div>
-            <div class="d-flex align-center ga-4">
-                <v-avatar rounded="0" size="100">
-                    <v-img :src="userAvatarUrl" style="border-radius: 16px" />
-                </v-avatar>
-                <div>
-                    <div class="font-weight-medium font-big">
-                        {{ userName }}
-                    </div>
-                    <div class="d-flex ga-2">
-                        <img :src="icon_github" />
-                        <span class="font-weight-medium font-mid">
-                            {{ userGithub }}
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div
-            class="d-flex justify-center align-center ml-4"
-            style="width: 115px"
-        >
-            <v-chip class="vertical-text text-h5" color="grey" variant="text">
-                {{ ticketNumber }}
-            </v-chip>
-        </div>
+        </v-img>
     </div>
 </template>
 
 <script>
 import icon_github from '@src/assets/svgs/icon-github.svg'
 import log_full_url from '@src/assets/svgs/logo-full.svg'
+import ticket_background_url from '@src/assets/svgs/pattern-ticket.svg'
 
 export default {
     props: {
@@ -55,16 +66,13 @@ export default {
         return {
             log_full_url,
             icon_github,
+            ticket_background_url,
         }
     },
 }
 </script>
 
 <style>
-.ticket-background {
-    background-image: url('../assets/svgs/pattern-ticket.svg');
-}
-
 .vertical-text {
     transform: rotate(90deg);
 }
--- src/main.js
+++ src/main.js
@@ -2,15 +2,22 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createVuetify } from 'vuetify'
-import * as components  from 'vuetify/components'
+import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
 
 const vuetify = createVuetify({
     components,
-    directives
+    directives,
+    display: {
+        mobileBreakpoint: 'sm',
+        thresholds: {
+            xs: 0,
+            sm: 768,
+            md: 1024,
+            lg: 1280,
+        },
+    },
 })
 
-createApp(App)
-    .use(vuetify)
-    .mount('#app')
+createApp(App).use(vuetify).mount('#app')
